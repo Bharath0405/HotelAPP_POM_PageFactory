5af3727b7ce71d40daa84f0e131c23448843b6d4
unknown <bharathreddy7690@gmail.com>
2019-02-15 13:06:34 +0530

    Log4j added to the project

--- src/test/java/com/hotelapp/qa/testcases/RegistrationPageTest.java
+++ src/test/java/com/hotelapp/qa/testcases/RegistrationPageTest.java
@@ -3,13 +3,16 @@ package com.hotelapp.qa.testcases;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.hotelapp.qa.base.TestBase;
 import com.hotelapp.qa.pages.RegistrationPage;
+import com.hotelapp.qa.util.TestUtil;
 
 public class RegistrationPageTest extends TestBase {
 	RegistrationPage registrationPage;
+	String sheetName="regData";
 	
 	public RegistrationPageTest() {
 		super();		
@@ -28,9 +31,16 @@ public class RegistrationPageTest extends TestBase {
 		String confText = registrationPage.regPageVerify();
 		Assert.assertEquals(confText, "New User Registration Form (Fields marked with Red asterix (*) are mandatory)");		
 	}
-	@Test(priority=2)
-	public void newUserRegFormTest() throws InterruptedException {
-		boolean regConfText = registrationPage.newUserRegForm("SelAutoKing1", "Selenium123", "Selenium123", "SelniumLearner", "adact2@yopmail.com");
+	
+	@DataProvider
+	public Object[][] getHotelappTestData(){
+		Object data[][] = TestUtil.getTestData(sheetName);
+		return data;
+	}
+	
+	@Test(priority=2, dataProvider= "getHotelappTestData")
+	public void newUserRegFormTest(String userName, String password, String rePassword, String fullName, String emailID) throws InterruptedException {
+		boolean regConfText = registrationPage.newUserRegForm(userName, password, rePassword, fullName, emailID);
 		Assert.assertEquals(regConfText, true);
 	}
 	@AfterMethod
